8b238884afb7f50e3affd8f8d299715e186ebcff
Adit Srivastava <aditsriv@live.com>
2018-08-19 01:48:44 +0530

    created listOptions, selectedFilter and result

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,8 +1,18 @@
-var viewModel = function() {
+function viewModel() {
     this.searchPlace = function() {
         search(this);
     };
-    this.options = ko.observable( new typeOfOptions() );
+    this.listOptions = ko.observableArray([
+        'Tourist Places',
+        'Pizza Places',
+        'Cafe',
+        'Library'
+    ]);
+    this.selectedFilter = ko.observable('Tourist Places');
+
+    this.result = ko.observableArray();
 }
+
+
 
 ko.applyBindings(new viewModel());
