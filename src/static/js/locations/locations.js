7ca72f6f7b004dde5c96558d3b000341ec59c433
Adit Srivastava <aditsriv@live.com>
2018-08-19 01:52:45 +0530

    Modified placeservice()

--- src/static/js/locations/locations.js
+++ src/static/js/locations/locations.js
@@ -1,23 +1,25 @@
-let typeOfOptions = function() {
-    this.listOptions = ko.observableArray([
-        'Tourist Places',
-        'Pizza Places',
-        'Cafe',
-        'Library'
-    ]);
-}
+function placeservice(self) {
+    let placeService = new google.maps.places.PlacesService(map);
+    let option = self.selectedFilter();
+    // emptying result ko.observableArray for every search
+    self.result.removeAll();
 
-
-function placeservice() {
-    let abc = new google.maps.places.PlacesService(map);
-    abc.textSearch(
+    placeService.textSearch(
         {
-            query: ('Tourist Places cafe in '+searchPlace),
+            query: (option+' in '+searchPlace),
             location: map.center,
             radius: '200'
         },
-        function(result, status, c) {
-            console.log(result)
+        function(results, status) {
+            results.forEach(function(x) {
+                console.log(x.name)
+                let result = {
+                    name: x.name,
+                    address: x.formatted_address,
+                    location: x.geometry.location
+                };
+                self.result.push(result);
+            });
         }
     );
 }
